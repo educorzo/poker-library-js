0b6a5d315a6021e9a98d3f8e37e30e684681dbca
Eduardo Corzo <edukorzo@gmail.com>
2020-08-26 21:37:03 +0200

    The base of the project

---
+++ HandsComparer.spec.js
@@ -0,0 +1,5 @@
+describe('Comparing two hands', function () {
+	it('check everything is working as expected', function (){
+		expect(true).toEqual(true);
+	});
+});
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      }
+    ]
+  }
+};

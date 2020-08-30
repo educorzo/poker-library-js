0b1725e7f3514e70f78016a134056b6224cc882b
Eduardo Corzo <edukorzo@gmail.com>
2020-08-30 12:04:50 +0200

    Refactor

--- src/HandsComparer.js
+++ src/HandsComparer.js
@@ -5,17 +5,23 @@ export default class HandsComparer {
 
    compare(hand1, hand2 ) {
     let hand1Resolved = HighCardResolver.resolveHighCard(hand1),
-      hand2Resolved = HighCardResolver.resolveHighCard(hand2),
-      index = 0,
+      hand2Resolved = HighCardResolver.resolveHighCard(hand2);
+
+    return this.resolveTie(hand1Resolved, hand2Resolved);
+  }
+
+  /*private*/
+  resolveTie(hand1, hand2) {
+    let index = 0,
       result = 0;
 
-      for(index; index < 5; index++) {
-        result = hand1Resolved.getCards()[index]
-          .compareTo(hand2Resolved.getCards()[index]);
-        if(result !== 0) {
-          return result;
-        }
+    for(index; index < 5; index++) {
+      result = hand1.getCards()[index]
+        .compareTo(hand2.getCards()[index]);
+      if(result !== 0) {
+        return result;
       }
+    }
 
     return result;
   }

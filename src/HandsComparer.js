ead8f85c12f57f31d4ca52f89177857c593c2782
Eduardo Corzo <edukorzo@gmail.com>
2020-09-02 18:41:48 +0200

    Refactor

--- src/HandsComparer.js
+++ src/HandsComparer.js
@@ -5,24 +5,39 @@ import PairResolver from './resolvers/PairResolver.js';
 export default class HandsComparer {
 
    compare(hand1, hand2) {
-     let bestCards1, bestCards2;
-      bestCards1 = PairResolver.tryResolvePair(hand1);
-      bestCards2 = PairResolver.tryResolvePair(hand2);
+     let bestCards1, bestCards2, result = 0;
 
-      if(bestCards1.length !== 0) {
-        if(bestCards2.length !== 0) {
-          return this.resolveByHighestCard(bestCards1, bestCards2);
-        }
+     result = this.resolvePair(hand1, hand2);
 
-        return 1;
-      } else if(bestCards2.length !== 0) {
-        return -1;
-      } else {
-        bestCards1 = HighCardResolver.resolveHighCard(hand1),
-        bestCards2 = HighCardResolver.resolveHighCard(hand2);
+     if(result == 0) {
+       return this.resolveHighCard(hand1, hand2);
+     } else {
+       return result;
+     }
+  }
 
-        return this.resolveByHighestCard(bestCards1, bestCards2);
-      }
+  resolvePair(hand1, hand2) {
+    let bestCards1, bestCards2;
+     bestCards1 = PairResolver.tryResolvePair(hand1);
+     bestCards2 = PairResolver.tryResolvePair(hand2);
+
+     if(bestCards1.length !== 0) {
+       if(bestCards2.length !== 0) {
+         return 0;
+       }
+       return 1;
+     } else if(bestCards2.length !== 0) {
+       return -1;
+     }
+     
+     return 0;
+  }
+
+  resolveHighCard(hand1, hand2) {
+    let bestCards1 = HighCardResolver.resolveHighCard(hand1),
+      bestCards2 = HighCardResolver.resolveHighCard(hand2);
+
+    return this.resolveByHighestCard(bestCards1, bestCards2);
   }
 
   /*private*/

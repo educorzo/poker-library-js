2e0c62729f0bf331733404180b6fa8e0a74f9f0d
Eduardo Corzo <edukorzo@gmail.com>
2020-09-09 10:43:02 +0200

    Adding missing restriction

--- src/parsers/HandParser.js
+++ src/parsers/HandParser.js
@@ -8,5 +8,9 @@ export function parseHand(handRaw) {
     cards.add(CardParser.parseCard(cardRaw));
   });
 
+  if(cards.length < 5) {
+    throw new Error('Hand with less than five cards is not allowed');
+  }
+
   return new Hand(cards);
 }
--- tests/parsers/HandParser.spec.js
+++ tests/parsers/HandParser.spec.js
@@ -7,4 +7,13 @@ describe('HandParser', function () {
     expect(hand.numberOfCards()).toEqual(5);
     expect(hand.toString()).toEqual('Ad Qh Js 3d 2d');
   });
+
+  describe('when there are less than five cards', function () {
+      it('should throw exception', () => {
+        let hand = HandParser.parseHand('2h 2s As Ad 3d');
+
+        expect(function(){HandParser.parseHand('2d Ad 3d Js')})
+          .toThrow(new Error('Hand with less than five cards is not allowed'));
+      });
+  });
 });
